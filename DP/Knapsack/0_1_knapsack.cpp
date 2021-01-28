de723ce1ae52b5a0e2f7cda71a867850b7ae4ab3
Shreyansh Gupta <shreyansh.gupta1299@gmail.com>
2021-01-28 14:32:44 +0530

    Unbounded knapsack algo corrected

---
+++ DP/Knapsack/0_1_knapsack.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int solveKnapsak(vector<int>& sz, vector<int>& val, int dp[2005][2005] , int s, int n) {
+    if(n < 0 || s < 0) return 0;
+    if(dp[n][s]!=-1) return dp[n][s];
+    int incs = 0, exl = 0;
+    if(s >= sz[n-1]) incs = val[n-1] + solveKnapsak(sz, val, dp, s-sz[n-1], n-1);
+    exl = solveKnapsak(sz, val, dp, s, n-1);
+    return dp[n][s] = max(incs, exl);
+}
+int main() {
+    #ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+    #endif
+    int t;
+    cin>>t;
+    vector<int> sz;
+    vector<int> val;
+    int dp[2005][2005] = {-1};
+    while(t--) {
+        int s,n;
+        cin>>s>>n;
+        for(int i=0;i<n;i++) {
+            int v,si;
+            cin>>si>>v;
+            sz.push_back(si);
+            val.push_back(v);
+        }
+        cout<<solveKnapsak(sz, val, dp, s, n)<<endl;
+        sz.clear();
+        val.clear();
+    }  
+    return 0;
+}
--- DP/Knapsack/0_n_knapsack.cpp
+++ DP/Knapsack/0_n_knapsack.cpp
@@ -1,38 +1,48 @@
 #include<bits/stdc++.h>
 #define ll long long int
 using namespace std;
-int solveKnapsak(vector<int>& sz, vector<int>& val, int dp[2005][2005] , int s, int n) {
-    if(n < 0 || s < 0) return 0;
-    if(dp[n][s]!=-1) return dp[n][s];
-    int incs = INT_MIN, exl = INT_MIN;
-    if(s >= sz[n-1]) incs = val[n-1] + solveKnapsak(sz, val, dp, s-sz[n-1], n-1);
-    exl = solveKnapsak(sz, val, dp, s, n-1);
-    return dp[n][s] = max(incs, exl);
+
+int knapsackTopDown(vector<int> &sz, vector<int> &value, vector<vector<int>>& dp, int s, int i) {
+	if(i>=value.size() or s<=0) return 0;
+	if(dp[s][i]!=-1) return dp[s][i];
+	int x = 0, y = 0;
+	if(s - sz[i] >= 0) x = knapsackTopDown(sz, value, dp, s-sz[i], i) + value[i];
+	y = knapsackTopDown(sz, value, dp, s, i+1);
+	return dp[s][i] = max(x, y);
 }
+
+int knapsackBottomUp(vector<int> &sz, vector<int> &value, int s) {
+	vector<int> dp(s+1, 0);
+	for(int i=0;i<sz.size();i++) {
+		for(int j=1;j<=s;j++)
+			if(j - sz[i] >= 0) dp[j] = max(dp[j], dp[j-sz[i]] + value[i]); 
+	}
+	return dp[s];
+}
+
 int main() {
-    #ifndef ONLINE_JUDGE
+	#ifndef ONLINE_JUDGE
     // for getting input from input.txt
     freopen("input.txt", "r", stdin);
     // for writing output to output.txt
     freopen("output.txt", "w", stdout);
     #endif
-    int t;
-    cin>>t;
-    vector<int> sz;
-    vector<int> val;
-    int dp[2005][2005] = {-1};
-    while(t--) {
-        int s,n;
-        cin>>s>>n;
-        for(int i=0;i<n;i++) {
-            int v,si;
-            cin>>si>>v;
-            sz.push_back(si);
-            val.push_back(v);
-        }
-        cout<<solveKnapsak(sz, val, dp, s, n)<<endl;
-        sz.clear();
-        val.clear();
-    }  
+    int n,s;
+    cin>>n>>s;
+    vector<int>sz;
+    vector<int> value;
+    vector<vector<int>> dp(s+1, vector<int>(n+1,-1));
+    for(int i=0;i<n;i++) {
+    	int no;
+    	cin>>no;
+    	sz.push_back(no);
+    }
+    for(int i=0;i<n;i++) {
+    	int no;
+    	cin>>no;
+    	value.push_back(no);
+    }
+    cout<<knapsackBottomUp(sz, value, s);
     return 0;
+
 }
