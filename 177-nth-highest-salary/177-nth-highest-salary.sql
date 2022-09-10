bfc9a2219b9ad133b4310a8ea57c779e2e0021b6
Shreyansh Gupta <49518586+mrFahrenhiet@users.noreply.github.com>
2022-09-10 10:29:40 +0530

    Time: 359 ms (72.88%), Space: 0B (100.00%) - LeetHub

--- 177-nth-highest-salary/177-nth-highest-salary.sql
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -3,8 +3,7 @@ BEGIN
   DECLARE M INT;
   SET M = N-1;
   RETURN (
-      IFNULL((
-      SELECT distinct salary from Employee
-      ORDER BY salary DESC LIMIT 1 OFFSET M), NULL)
+      SELECT DISTINCT salary from Employee
+      ORDER BY salary DESC LIMIT M,1
   );
 END
