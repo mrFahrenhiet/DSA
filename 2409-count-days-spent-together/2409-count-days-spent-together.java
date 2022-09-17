52ff08f5ac78657f7cb30ff87075764b9c47db95
Shreyansh Gupta <49518586+mrFahrenhiet@users.noreply.github.com>
2022-09-17 23:40:00 +0530

    Time: 16 ms (10.00%), Space: 42.2 MB (40.00%) - LeetHub

---
+++ 2409-count-days-spent-together/2409-count-days-spent-together.java
@@ -0,0 +1,21 @@
+class Solution {
+    private int getDays(String date, int[] nosDays) {
+        String[] dateSp = date.split("-");
+        int days = nosDays[Integer.parseInt(dateSp[0]) - 1] + Integer.parseInt(dateSp[1]);
+        return days;
+    }
+    
+    public int countDaysTogether(String arriveAlice, String leaveAlice, String arriveBob, String leaveBob) {
+        int[] nosDays = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        for(int i=1;i<nosDays.length;i++)
+            nosDays[i] += nosDays[i-1];
+        
+        int arrAlice = getDays(arriveAlice, nosDays);
+        int arrBob = getDays(arriveBob, nosDays);
+        int levAlice = getDays(leaveAlice, nosDays);
+        int levBob = getDays(leaveBob, nosDays);
+        System.out.println(arrAlice + " " + arrBob + " " + levAlice + " " + levBob);
+        int res = Math.min(levAlice, levBob) - Math.max(arrAlice, arrBob) + 1;
+        return res < 0 ? 0 : res;
+    }  
+}
