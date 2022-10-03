e9f3bd5ac2c624c3e64fbdc0d5c708178decd714
Shreyansh Gupta <49518586+mrFahrenhiet@users.noreply.github.com>
2022-10-03 22:10:50 +0530

    Time: 74 ms (31.44%), Space: 117.9 MB (43.98%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -0,0 +1,43 @@
+class Points {
+    public int x;
+    public int y;
+    public double vec;
+    
+    public Points(int xc, int yc) {
+        this.x = xc;
+        this.y = yc;
+        this.vec = Math.sqrt(xc*xc + yc*yc);
+    }
+}
+class PointsComparator implements Comparator<Points> {
+    @Override
+    public int compare(Points p1, Points p2) {
+        return p2.vec > p1.vec ? 1 : p2.vec < p1.vec ? -1 : 0;
+    }
+}
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<Points> pq = new PriorityQueue<Points>(k, new PointsComparator());
+        for(int i=0;i<k;i++) {
+            pq.add(new Points(points[i][0], points[i][1]));
+        }
+        //System.out.println("PriorityQueue - " + pq);
+        for(int i=k;i<points.length;i++) {
+            Points newPoint = new Points(points[i][0], points[i][1]);
+            Points currWorst = pq.peek();
+            //System.out.println(newPoint.vec + " " + currWorst.vec + " " + currWorst.x + " " +currWorst.y);
+            if(newPoint.vec < currWorst.vec) {
+                pq.poll();
+                pq.add(newPoint);
+            }
+        }
+        int[][] res = new int[k][2];
+        int i = 0;
+        while(pq.size() > 0) {
+            Points point = pq.poll();
+            res[i] = new int[]{point.x, point.y};
+            i++;
+        }
+        return res;
+    }
+}
