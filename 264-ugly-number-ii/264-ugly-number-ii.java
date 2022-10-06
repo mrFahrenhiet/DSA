ce32164bd186c72d0cc7be6bc8979be27fccedaa
Shreyansh Gupta <49518586+mrFahrenhiet@users.noreply.github.com>
2022-10-06 23:16:36 +0530

    Time: 6 ms (52.74%), Space: 42.6 MB (41.32%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.java
+++ 264-ugly-number-ii/264-ugly-number-ii.java
@@ -1,26 +1,17 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        long res = 1;
-        if(n == 1) return 1;
+        int idx2 = 1, idx3 = 1, idx5 = 1;
+        int[] dp = new int[n+1];
+        dp[1] = 1;        
         
-        HashMap<Long, Boolean> isDone = new HashMap<>();
-        PriorityQueue<Long> pq = new PriorityQueue<Long>();
-        pq.add((long)1);
-        isDone.put((long)1, true);
-        
-        for(long i=1;i<=n;i++) {
-            long cur = pq.poll();
-            res = cur;
-            
-            if(!isDone.containsKey(cur * 2)) pq.add(cur * 2);
-            if(!isDone.containsKey(cur * 3)) pq.add(cur * 3);
-            if(!isDone.containsKey(cur * 5)) pq.add(cur * 5);
-            
-            isDone.put(cur * 2, true);
-            isDone.put(cur * 3, true);
-            isDone.put(cur * 5, true);
+        for(int i=2;i<=n;i++) {
+            int cur = Math.min(2*dp[idx2], Math.min(3*dp[idx3], 5*dp[idx5]));
+            dp[i] = cur;
+            if(cur == 2 * dp[idx2]) idx2++;
+            if(cur == 3 * dp[idx3]) idx3++;
+            if(cur == 5 * dp[idx5]) idx5++;
         }
         
-        return (int) (res);
+        return dp[n];
     }
 }
