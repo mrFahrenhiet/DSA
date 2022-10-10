c9bcee2b65449d7bb889c689256ea1d0d6654562
Shreyansh Gupta <49518586+mrFahrenhiet@users.noreply.github.com>
2022-10-10 15:16:24 +0530

    Time: 4 ms (86.20%), Space: 46.6 MB (82.22%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,24 +9,28 @@
  * }
  */
 class Solution {
+    private ListNode merge(ListNode l1, ListNode l2) {
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        
+        if(l1.val > l2.val) {
+            l2.next = merge(l1, l2.next);
+            return l2;
+        }
+        else {
+            l1.next = merge(l1.next, l2);
+            return l1;
+        }  
+    }
+    private ListNode partition(ListNode[] lists, int st, int en) {
+        if(st == en) return lists[st];
+        if(st > en) return null;
+        int mid = (st + (en - st)/2);
+        ListNode l1 = partition(lists, st, mid);
+        ListNode l2 = partition(lists, mid + 1, en);
+        return merge(l1, l2);
+    }
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>((a,b) -> a.val - b.val);
-        ListNode res = new ListNode(-1);
-        ListNode traveler = res;
-        
-        for(ListNode list: lists) {
-            if(list != null) pq.add(list);
-        }
-        
-        while(pq.size() > 0) {
-            ListNode cur = pq.poll();
-            if(cur != null) {
-                traveler.next = cur;
-                traveler = traveler.next;
-                if(cur.next != null) pq.add(cur.next);
-            }
-        }
-        
-        return res.next;
+        return partition(lists, 0, lists.length-1);
     }
 }
