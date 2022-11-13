449d42a208596176cd64131af55e66bc010f8549
Shreyansh Gupta <49518586+mrFahrenhiet@users.noreply.github.com>
2022-11-13 12:16:38 +0530

    Time: 30 ms (18.59%), Space: 46.8 MB (64.38%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,31 +1,25 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int[][] dp = new int[n][k+3];
+        int[][] dp = new int[n][k+2];
         for(int i=0;i<n;i++) {
-            for(int j=0;j<k+3;j++)
+            for(int j=0;j<k+2;j++)
                 dp[i][j] = Integer.MAX_VALUE;
         }
         
-        dp[src][0] = 0;
+        for(int i=0;i<k+2;i++) dp[src][0] = 0;
         
-        for(int i=1;i<n;i++) {
+        for(int i=1;i<k+2;i++) {
             for(int j=0;j<flights.length;j++) {
                 int s = flights[j][0];
                 int d = flights[j][1];
                 int p = flights[j][2];
-                
-                for(int l=0;l<k+1;l++) {
-                    if(dp[s][l] != Integer.MAX_VALUE &&
-                       dp[d][l+1] > dp[s][l] + p) {
-                        dp[d][l+1] = dp[s][l] + p;
-                    }
-                }
+                if(dp[s][i-1] != Integer.MAX_VALUE)
+                    dp[d][i] = Math.min(dp[d][i], dp[s][i-1] + p);
             }
         }
-        int res = Integer.MAX_VALUE;
         
-        for(int l=0;l<k+2;l++)
-            res = Math.min(res, dp[dst][l]);
+        int res = Integer.MAX_VALUE;
+        for(int i=0;i<k+2;i++) res = Math.min(res, dp[dst][i]);
         
         return res == Integer.MAX_VALUE ? -1 : res;
     }
