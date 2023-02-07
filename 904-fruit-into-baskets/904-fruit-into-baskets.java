7e0025df60a511d6693e823e7c94e6beae169194
Shreyansh Gupta <49518586+mrFahrenhiet@users.noreply.github.com>
2023-02-08 00:53:54 +0530

    Time: 46 ms (76.26%), Space: 50.5 MB (88.15%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.java
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -13,7 +13,6 @@ class Solution {
                 }
                 
                 fruitTypes.remove(minKey);
-                System.out.println(minKey);
                 j = minIdx+1;
             }
             
