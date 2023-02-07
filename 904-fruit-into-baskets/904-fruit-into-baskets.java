18aebcf3533ed2adf4cc944d221e51fe5baf972e
Shreyansh Gupta <49518586+mrFahrenhiet@users.noreply.github.com>
2023-02-08 00:52:41 +0530

    Time: 258 ms (5.03%), Space: 51.1 MB (79.19%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        HashMap<Integer, Integer> fruitTypes = new HashMap<>();
+        int res = 1;
+        for(int i=0, j=0;i<fruits.length;i++) {
+            if(fruitTypes.size() >= 2 && !fruitTypes.containsKey(fruits[i])) {
+                int minIdx = fruits.length, minKey = -1;
+                for(int key: fruitTypes.keySet()) {
+                    if(minIdx > fruitTypes.get(key)) {
+                        minIdx = fruitTypes.get(key);
+                        minKey = key;
+                    }
+                }
+                
+                fruitTypes.remove(minKey);
+                System.out.println(minKey);
+                j = minIdx+1;
+            }
+            
+            fruitTypes.put(fruits[i], i);
+            res = Math.max(res, i - j + 1);
+
+        }
+        
+        return res;
+    }
+}
