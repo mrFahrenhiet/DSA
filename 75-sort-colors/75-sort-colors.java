3d4d331156afe8333a1cbe52c3dcfaf395ae1e8f
Shreyansh Gupta <49518586+mrFahrenhiet@users.noreply.github.com>
2023-03-28 23:53:06 +0530

    Time: 0 ms (100.00%), Space: 41 MB (38.64%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,18 +1,26 @@
 class Solution {
-    public void sortColors(int[] nums) {
-        int[] freq = new int[3];
-        
-        for(int i=0;i<nums.length;i++) freq[nums[i]]++;
-        
-        int j = 0;
-        for(int i=0;i<3;i++) {
-            while(freq[i] > 0) {
-                nums[j] = i;
-                freq[i]--;
-                j++;
-            }
-        }
-        
+    private void swap(int[] arr, int idx1, int idx2) {
+        int temp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = temp;
         return;
+    }
+    public void sortColors(int[] nums) {
+        int zeros_idx = 0, twos_idx = nums.length-1;
+        
+        for(int i=0;i<=twos_idx;i++) {
+            if(nums[i] == 0) {
+                swap(nums, i, zeros_idx);
+                zeros_idx++;
+            }
+            
+            if(nums[i] == 2) {
+                swap(nums, i, twos_idx);
+                twos_idx--;
+                i--;
+            }
+            
+            
+        }
     }
 }
