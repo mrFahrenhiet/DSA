abd7332b34ba2142cec7016587e49c62f4bce042
Shreyansh Gupta <49518586+mrFahrenhiet@users.noreply.github.com>
2023-05-21 23:44:10 +0530

    Time: 106 ms (42.62%), Space: 50.4 MB (32.78%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,45 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        
+        Arrays.sort(nums);
+        
+        for(int num: nums) System.out.print(num + " ");
+        System.out.println();
+        
+        for(int i=0;i<nums.length;i++) {
+            List<Integer> temp = new ArrayList<Integer>();
+            
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            
+            temp.add(nums[i]);
+            
+            int target = -1 * nums[i], st = i + 1, en = nums.length - 1;
+                        
+            while(st < en) {
+                while(st < en && st > i+1 && nums[st] == nums[st-1]) st++;
+                if(st >= en) break;
+                //if(st < en && en < nums.length - 1 && nums[en] == nums[en + 1]) en--;
+                
+                if(nums[st] + nums[en] == target) {
+                    temp.add(nums[st]);
+                    temp.add(nums[en]);
+                    
+                    res.add(new ArrayList<Integer>(temp));
+                    
+                    temp.remove(temp.size() - 1);
+                    temp.remove(temp.size() - 1);
+                    
+                    st++;
+                    en--;
+                }
+                else if(nums[st] + nums[en] > target) en--;
+                else st++;
+            }
+            
+            temp.remove(temp.size() - 1);
+        }
+        
+        return res;
+    }
+}
