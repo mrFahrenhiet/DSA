bf7bf53db515209e4c859ad463718080889ec2ce
Shreyansh Gupta <49518586+mrFahrenhiet@users.noreply.github.com>
2023-07-02 00:12:35 +0530

    Time: 719 ms (5.03%), Space: 40.2 MB (46.57%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,32 @@
+class Solution {
+    private boolean isRightMove(char[][] board, int row, int col, char c) {
+        for(int i = 0; i < 9; i++) {
+            if(board[i][col] != '.' && board[i][col] == c) return false;
+            if(board[row][i] != '.' && board[row][i] == c) return false;
+            if(board[3 * (row / 3) + i / 3][ 3 * (col / 3) + i % 3] != '.' && 
+board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
+        }
+        return true;        
+    }
+    
+    private boolean helper(char[][] board, int i, int j) {
+        if(i >= board.length) return true;
+        if(j >= board[0].length) return helper(board, i+1, 0);
+        if(board[i][j] != '.') return helper(board, i, j+1);
+        
+        for(int k=1;k<=9;k++) {
+            char x = (char)(k + (int)'0');
+            if(isRightMove(board, i, j, x)) {
+                board[i][j] = x;
+                if(helper(board, i, j+1)) return true;
+                board[i][j] = '.';
+            }
+        }     
+        return false;
+    }
+    
+    public void solveSudoku(char[][] board) {
+        boolean x = helper(board, 0, 0);
+        return;
+    }
+}
