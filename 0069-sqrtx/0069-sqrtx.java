c39d9dd4a65ebb4badaa68cd73d4915c6ecb5041
Shreyansh Gupta <49518586+mrFahrenhiet@users.noreply.github.com>
2023-07-10 22:22:39 +0530

    Time: 1 ms (100.00%), Space: 40.2 MB (18.88%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -3,18 +3,15 @@ class Solution {
         int low = 1;
         int high = x;
         
-        int res = 0;
+        int mid = 0;
         
         while(low <= high) {
-            int mid = (low + (high - low) / 2);
-            if(mid == x / mid) return mid;
+            mid = (low + (high - low) / 2);
+
             if(mid > x / mid) high = mid - 1;
-            else {
-                res = mid;
-                low = mid + 1;
-            }
+            else low = mid + 1;
         }
         
-        return res;
+        return low - 1;
     }
 }
