1ca691260f44b28ea45987a03d317ede1b614fd9
Shreyansh Gupta <49518586+mrFahrenhiet@users.noreply.github.com>
2023-07-24 15:00:27 +0530

    Time: 17 ms (80.80%), Space: 44.1 MB (89.18%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,27 +1,13 @@
 class Solution {
     private boolean canKokoFinish(int[] piles, int h, int k) {
-        // int i = 0;
         for(int pile: piles) {
             h -= (pile / k) + (pile % k == 0 ? 0 : 1);
             if(h < 0) return false;
         }
         
         return true;
-//         while(i < piles.length) {
-//             if(piles[i] > k) {
-//                 h -= (piles[i] / k) + ((piles[i] % k) == 0 ? 0: 1);
-//                 i++;
-//             }
-//             else {
-//                 i++;
-//                 h--;
-//             }
-            
-//             if(h < 0) return false;
-//         }
-        
-//         return true;
     }
+    
     public int minEatingSpeed(int[] piles, int h) {
         int low = 1, high = 0;
         for(int pile: piles) high = Math.max(pile, high);
