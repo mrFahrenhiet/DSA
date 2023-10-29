38ba0dbdcb2c7a46af2bd2f4b65fd028e20a2cec
Shreyansh Gupta <49518586+mrFahrenhiet@users.noreply.github.com>
2023-10-29 11:24:34 +0530

    Time: 384 ms (50.00%), Space: 74.6 MB (50.00%) - LeetHub

--- 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
+++ 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.java
@@ -1,28 +1,29 @@
 class Solution {
-    private int solve(List<Integer> nums, int i, int target, Integer[][] dp) {
-        if(target == 0) {
-            return 1;
-        }
-        if(target < 0 || i < 0) {
-            return 0;
-        }
-        
-        if(dp[i][target] != null) {
-            return dp[i][target];
-        }
-        
-        int include = solve(nums, i-1, target-nums.get(i), dp);
-        int dontInclude = solve(nums, i-1, target, dp);
-        
+    private int helper(List<Integer> nums, Integer[][] dp, int cur, int target) {
+        if(cur >= nums.size() && target > 0) return Integer.MIN_VALUE;
+        if(nums.size() <= cur || target == 0) return 0;
+        if(dp[cur][target] != null) return dp[cur][target];
         int res = 0;
-        if(include == 0 && dontInclude == 0) {
-            res = 0;
-        } else {
-            res = Math.max(include+1, dontInclude);
+        int incl = 0;
+        
+        if(target - nums.get(cur) >= 0) {
+            incl = helper(nums, dp, cur+1, target - nums.get(cur)) + 1;
         }
-        return dp[i][target] = res;
+        
+        int excl = helper(nums, dp, cur+1, target);
+        
+        if(excl < 0 && incl == 0 || incl < 0 && excl == 0)
+            return dp[cur][target] = Integer.MIN_VALUE;
+        
+        res += Math.max(incl, excl);
+        
+        return dp[cur][target] = res;
     }
     public int lengthOfLongestSubsequence(List<Integer> nums, int target) {
-        return solve(nums, nums.size()-1, target, new Integer[nums.size()][target+1])-1;
+        Integer[][] dp = new Integer[nums.size()][target + 1];
+
+        int res = helper(nums, dp, 0, target);
+        
+        return res <= 0 ? -1 : res;
     }
 }
